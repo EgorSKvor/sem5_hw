43cd97f05e89924680efe4c0ba64ba5b61eb8153
Egor <ee3068316@gmail.com>
2022-09-20 15:48:50 +0300

    add ex1

--- ex001/Program.cs
+++ ex001/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿int[] New_array = GetArray(3, 100, 999);
+
+int [] Find_ev_num(int size, int inNum, int[] arr)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if( arr[i] / 2 == 0)
+        {
+            inNum += 1;
+        }
+    }
+    return result;
+}
+
+int[] GetArray (int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+
+System.Console.WriteLine(New_array);
